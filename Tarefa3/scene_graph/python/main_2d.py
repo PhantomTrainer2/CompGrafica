436ffdd580385d3a558633d9a3f1be2636d150d4
Breno <gallobreno@gmail.com>
2025-09-30 09:20:28 -0300

    venus

--- Tarefa3/scene_graph/python/main_2d.py
+++ Tarefa3/scene_graph/python/main_2d.py
@@ -1,100 +1,139 @@
-from OpenGL.GL import *
 import glfw
-import random as rd
+import time
+import os
+from OpenGL.GL import *
+import glm
 
-from camera2d import *
-from color import *
-from transform import *
-from quad import *
-from triangle import *
-from node import *
-from shader import *
-from scene import *
-from engine import *
+from scene import Scene
+from state import State
+from node import Node
+from transform import Transform
+from material import Material
+from shader import Shader
+from camera2d import Camera2D
+from luxor.disk import Disk
+from solar_system_animation import SolarSystemAnimation
 
-class MovePointer(Engine):
-  def __init__ (self, trf):
-    self.trf = trf
-  def Update (self, dt):
-    self.trf.Rotate(6*dt,0,0,-1)
+scene = None
+state = None
+last_time = 0.0
 
-def initialize ():
-  # set background color: white 
-  glClearColor(0.8,1.0,1.0,1.0)
-  # enable depth test 
-  glEnable(GL_DEPTH_TEST)
+def init_app():
+    global scene, state
 
-  # create objects
-  global camera
-  camera = Camera2D(0,10,0,10)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(script_dir)
+    shaders_path = os.path.join(project_root, 'shaders')
 
-  trf1 = Transform()
-  trf1.Translate(3,3,-0.5)
-  trf1.Scale(4,4,1)
-  face = Node(trf=trf1,apps=[Color(1,1,1)],shps=[Quad()])
-  trf2 = Transform()
-  trf2.Translate(5,5,0)
-  trf3 = Transform()
-  trf3.Scale(0.1,2,1)
-  pointer = Node(trf=trf2,nodes=[Node(trf=trf3,apps=[Color(1,0,0)],shps=[Triangle()])])
+    illum_shader = Shader()
+    vertex_shader_file = os.path.join(shaders_path, 'ilum_vert', 'vertex.glsl')
+    fragment_shader_file = os.path.join(shaders_path, 'ilum_vert', 'fragment.glsl')
+    illum_shader.AttachVertexShader(vertex_shader_file)
+    illum_shader.AttachFragmentShader(fragment_shader_file)
+    illum_shader.Link()
 
-  shader = Shader()
-  shader.AttachVertexShader("../shaders/2d/vertex.glsl")
-  shader.AttachFragmentShader("../shaders/2d/fragment.glsl")
-  shader.Link()
+    illum_shader.UseProgram()
+    light_pos = glm.vec4(0.0, 0.0, 1.0, 0.0)
+    light_amb = glm.vec4(0.3, 0.3, 0.3, 1.0) 
+    light_dif = glm.vec4(1.0, 1.0, 1.0, 1.0)
+    light_spe = glm.vec4(1.0, 1.0, 1.0, 1.0)
+    illum_shader.SetUniform("lpos", light_pos)
+    illum_shader.SetUniform("lamb", light_amb)
+    illum_shader.SetUniform("ldif", light_dif)
+    illum_shader.SetUniform("lspe", light_spe)
+    glUseProgram(0)
 
-  # build scene
-  root = Node(shader, nodes = [face,pointer])
-  global scene 
-  scene = Scene(root)
-  scene.AddEngine(MovePointer(trf2))
+    sun_material = Material(1.0, 1.0, 0.0)
+    earth_material = Material(0.2, 0.2, 1.0)
+    moon_material = Material(1.0, 1.0, 1.0)
+    venus_material = Material(0.0, 1.0, 0.2)
 
-def update (dt):
-  scene.Update(dt)
+    disk_shape = Disk(segments=100)
 
-def display ():
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) 
-  scene.Render(camera)
+    root_node = Node()
+    sun_scale = Transform()
+    sun_scale.Scale(3.0, 3.0, 3.0)
+    sun_spin = Transform() 
 
-def keyboard (win, key, scancode, action, mods):
-   if key == glfw.KEY_Q and action == glfw.PRESS:
-      glfw.set_window_should_close(win,glfw.TRUE)
+    earth_orbit = Transform()
+    earth_translate = Transform()
+    earth_translate.Translate(5.0, 0, 0)
+    earth_scale = Transform()
+    earth_scale.Scale(0.8, 0.8, 0.8)
+    earth_spin = Transform() 
+    
+    venus_orbit = Transform()
+    venus_translate = Transform()
+    venus_translate.Translate(2.0, 0, 0)
+    venus_scale = Transform()
+    venus_scale.Scale(0.4, 0.4, 0.4)
+    venus_spin = Transform() 
+
+    moon_orbit = Transform()
+    moon_translate = Transform()
+    moon_translate.Translate(1.0, 0, 0)
+    moon_scale_trf = Transform()
+    moon_scale_trf.Scale(0.3, 0.3, 0.3)
+    
+    moon_leaf_node = Node(trf=moon_scale_trf, apps=[moon_material], shps=[disk_shape])
+    moon_translate_node = Node(trf=moon_translate, nodes=[moon_leaf_node])
+    moon_orbit_node = Node(trf=moon_orbit, nodes=[moon_translate_node])
+    
+    earth_spin_node = Node(trf=earth_spin, apps=[earth_material], shps=[disk_shape])
+    earth_scale_node = Node(trf=earth_scale, nodes=[earth_spin_node])
+    earth_translate_node = Node(trf=earth_translate, nodes=[earth_scale_node, moon_orbit_node])
+    earth_orbit_node = Node(trf=earth_orbit, nodes=[earth_translate_node])
+
+    venus_spin_node = Node(trf=venus_spin, apps=[venus_material], shps=[disk_shape])
+    venus_scale_node = Node(trf=venus_scale, nodes=[venus_spin_node])
+    venus_translate_node = Node(trf=venus_translate, nodes=[venus_scale_node])
+    venus_orbit_node = Node(trf=venus_orbit, nodes=[venus_translate_node])
+    
+    sun_spin_node = Node(shader=illum_shader, trf=sun_spin, apps=[sun_material], shps=[disk_shape])
+    sun_scale_node = Node(trf=sun_scale, nodes=[sun_spin_node])
+    earth_orbit_node.SetShader(illum_shader)
+    venus_orbit_node.SetShader(illum_shader)
+
+
+    root_node.AddNode(earth_orbit_node)
+    root_node.AddNode(venus_orbit_node)
+    root_node.AddNode(sun_scale_node)
+
+    scene = Scene(root_node)
+    engine = SolarSystemAnimation(earth_orbit, moon_orbit, earth_spin, sun_spin, venus_orbit, venus_spin)
+    scene.AddEngine(engine)
+    
+    camera = Camera2D(xmin=-5, xmax=5, ymin=-5, ymax=5)
+    state = State(camera)
+
+def update_and_draw():
+    global last_time
+    current_time = glfw.get_time()
+    dt = current_time - last_time
+    last_time = current_time
+    glClearColor(0.0, 0.0, 0.1, 1.0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    if scene and state:
+        for e in scene.engines:
+            e.Update(current_time)
+        scene.Render(state.camera)
 
 def main():
-    # Initialize the library
+    global last_time
     if not glfw.init():
         return
-    # Create a windowed mode window and its OpenGL context
-    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR,4)
-    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR,1)
-    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT,GL_TRUE)
-    win = glfw.create_window(600, 600, "2D scene", None, None)
-    if not win:
+    window = glfw.create_window(800, 600, "Sistema Solar 2D", None, None)
+    if not window:
         glfw.terminate()
         return
-    glfw.set_key_callback(win,keyboard)
-
-    # Make the window's context current
-    glfw.make_context_current(win)
-    print("OpenGL version: ",glGetString(GL_VERSION))
-
-    initialize()
-
-    # Loop until the user closes the window
-    t0 = glfw.get_time()
-    while not glfw.window_should_close(win):
-        t = glfw.get_time()
-        update(t-t0)
-        t0 = t
-        display()
-
-        # Swap front and back buffers
-        glfw.swap_buffers(win)
-
-        # Poll for and process events
+    glfw.make_context_current(window)
+    glEnable(GL_DEPTH_TEST)
+    init_app()
+    last_time = glfw.get_time()
+    while not glfw.window_should_close(window):
+        update_and_draw()
+        glfw.swap_buffers(window)
         glfw.poll_events()
-
     glfw.terminate()
 
 if __name__ == "__main__":
--- Tarefa3/scene_graph/python/solar_system_animation.py
+++ Tarefa3/scene_graph/python/solar_system_animation.py
@@ -3,29 +3,39 @@ from engine import Engine
 from transform import Transform
 
 class SolarSystemAnimation(Engine):
-    def __init__(self, earth_orbit_transform, moon_orbit_transform, earth_spin_transform, sun_spin_transform):
+    def __init__(self, earth_orbit_transform, moon_orbit_transform, earth_spin_transform, sun_spin_transform, venus_orbit_transform, venus_spin_transform):
         self.earth_orbit_transform = earth_orbit_transform
         self.moon_orbit_transform = moon_orbit_transform
         self.earth_spin_transform = earth_spin_transform
         self.sun_spin_transform = sun_spin_transform
+        self.venus_orbit_transform = venus_orbit_transform
+        self.venus_spin_transform = venus_spin_transform
 
     def Update(self, time):
         self.earth_orbit_transform.LoadIdentity()
         self.moon_orbit_transform.LoadIdentity()
         self.earth_spin_transform.LoadIdentity()
         self.sun_spin_transform.LoadIdentity()
+        self.venus_orbit_transform.LoadIdentity()
+        self.venus_spin_transform.LoadIdentity()
 
         earth_orbit_speed = 0.5
         moon_orbit_speed = 3.0
         earth_spin_speed = 2.0
         sun_spin_speed = 0.2
+        venus_orbit_speed = -1.5
+        venus_spin_speed = 2.0
 
         earth_orbit_angle_rad = time * earth_orbit_speed
         moon_orbit_angle_rad = time * moon_orbit_speed
         earth_spin_angle_rad = time * earth_spin_speed
         sun_spin_angle_rad = time * sun_spin_speed
+        venus_orbit_angle_rad = time * venus_orbit_speed
+        venus_spin_angle_rad = time * venus_spin_speed
         
         self.earth_orbit_transform.Rotate(math.degrees(earth_orbit_angle_rad), 0, 0, 1)
         self.moon_orbit_transform.Rotate(math.degrees(moon_orbit_angle_rad), 0, 0, 1)
         self.earth_spin_transform.Rotate(math.degrees(earth_spin_angle_rad), 0, 0, 1)
         self.sun_spin_transform.Rotate(math.degrees(sun_spin_angle_rad), 0, 0, 1)
+        self.venus_orbit_transform.Rotate(math.degrees(venus_orbit_angle_rad), 0, 0, 1)
+        self.venus_spin_transform.Rotate(math.degrees(venus_spin_angle_rad), 0, 0, 1)
