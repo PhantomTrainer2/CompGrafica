30addd85a88dce13c8b05b55ca37d8f75fa731c4
Breno <gallobreno@gmail.com>
2025-10-13 09:16:54 -0300

    tesgte1

--- Tarefa4/shaders/ilum_vert/vertex.glsl
+++ Tarefa4/shaders/ilum_vert/vertex.glsl
@@ -3,37 +3,24 @@
 layout(location = 0) in vec4 coord;
 layout(location = 1) in vec3 normal;
 
-uniform mat4 Mv; 
-uniform mat4 Mn; 
-uniform mat4 Mvp;
+uniform mat4 Mv;  // Model-View matrix
+uniform mat4 Mn;  // Normal matrix
+uniform mat4 Mvp; // Model-View-Projection matrix
 
-uniform vec4 lpos;  // light pos in eye space
-uniform vec4 lamb;
-uniform vec4 ldif;
-uniform vec4 lspe;
-
-uniform vec4 mamb;
-uniform vec4 mdif;
-uniform vec4 mspe;
-uniform float mshi;
-
-out vec4 color;
+// Dados que serão interpolados e enviados ao fragment shader
+out data {
+  vec3 position_eye;  // Posição do vértice no espaço da câmera
+  vec3 normal_eye;    // Normal no espaço da câmera
+} v;
 
 void main (void) 
 {
-  vec3 veye = vec3(Mv*coord);
-  vec3 light;
-  if (lpos.w == 0) 
-    light = normalize(vec3(lpos));
-  else 
-    light = normalize(vec3(lpos)-veye); 
-  vec3 neye = normalize(vec3(Mn*vec4(normal,0.0f)));
-  float ndotl = dot(neye,light);
-  color = mamb*lamb + mdif * ldif * max(0,ndotl); 
-  if (ndotl > 0) {
-    vec3 refl = normalize(reflect(-light,neye));
-    color += mspe * lspe * pow(max(0,dot(refl,normalize(-veye))),mshi); 
-  }
-  gl_Position = Mvp*coord; 
+  // Transforma a posição do vértice para o espaço da câmera
+  v.position_eye = vec3(Mv * coord);
+  
+  // Transforma a normal para o espaço da câmera
+  v.normal_eye = normalize(vec3(Mn * vec4(normal, 0.0)));
+  
+  // Calcula a posição final do vértice
+  gl_Position = Mvp * coord; 
 }
-
